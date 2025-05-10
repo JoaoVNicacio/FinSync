2590b8e0a94c650fa42a5ff8193c9e3be495b1a9
João Victor Nicacio <joaonicks99@gmail.com>
2025-05-10 20:53:03 -0300

    Revert "feat & refac: Added gRPC to API and adjusted entity relations in domain."

--- FinSync.Finances/Src/FinSync.Finances.Domain/Usings.cs
+++ FinSync.Finances/Src/FinSync.Finances.Domain/Usings.cs
@@ -1,3 +1,2 @@
 global using FinSync.Finances.Domain.Enums;
 global using FinSync.Finances.Domain.Entities;
-global using System.Collections.ObjectModel;
--- Libs/FinSync.Core/Validation/Response/ValidationError.cs
+++
@@ -1,8 +0,0 @@
-namespace FinSync.Core.Validation.Response;
-
-public record ValidationError(
-  object? target,
-  string property,
-  object? value,
-  Dictionary<string, string> constraints,
-  IEnumerable<ValidationError>children);
